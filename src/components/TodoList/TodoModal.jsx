11a0672303f51596a3e04dad3f85d330954e47ae
hatrungt1n <hatrungtin2k2@gmail.com>
2023-02-21 14:05:00 +0700

    UI desktop done

--- src/components/TodoList/TodoModal.jsx
+++ src/components/TodoList/TodoModal.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoModal = ({title}) => {
+const TodoModal = ({ title }) => {
+  const [show, setShow] = useState(false)
+
   return (
-    <div className="form">
-      <div className="title">{title}</div>
+    <div className="formModal">
+      <div className="form">
+        <div className="title">{title}</div>
 
-      <div className="btn">
-        <button className="detailBtn">Detail</button>
-        <button className="removeBtn">Remove</button>
+        <div className="btn">
+          <button className="detailBtn" onClick={e => setShow(!show)}>Detail</button>
+          <button className="removeBtn">Remove</button>
+        </div>
+      </div>
+
+      <div className={show ? "detailShow" : "detailHide"}>
+        <input
+          className="input"
+          id="newTaskInput"
+          type="text"
+          placeholder=""
+        />
+
+        <div className="description">
+          <h2>Description</h2>
+          <textarea className="desInput" cols="30" rows="10"></textarea>
+        </div>
+
+        <div className="props">
+          <div className="dueDate">
+            <h2>Due Date</h2>
+            <input className="date" type="date" />
+          </div>
+
+          <div className="priority">
+            <h2>Priority</h2>
+            <select className="prio" id="prio">
+              <option value="Low">Low</option>
+              <option value="Normal" selected>
+                Normal
+              </option>
+              <option value="High">High</option>
+            </select>
+          </div>
+        </div>
+
+        <button className="updateBtn">Update</button>
       </div>
     </div>
   );
